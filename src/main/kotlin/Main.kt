ea9fb49c55e88f30c3e1723f75a854020171df22
Nikita-Vesel0v <nikiveselov2004@gmail.com>
2024-05-20 11:52:02 +0300

    create develop branch

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,17 @@
-package org.example
+import kotlin.io.path.Path
+
+import kotlin.io.path.forEachLine
+import kotlin.io.path.readLines
+import kotlin.io.path.readText
 
 fun main() {
-    println("Hello World!")
+    val phoneBook = Path("src/main/resources/directory.txt")
+    val peopleFind = Path("src/main/resources/find.txt")
+    val listPhoneBook = phoneBook.readText()
+    var count = 0
+    println("Start searching...")
+    val startTime = System.currentTimeMillis()
+    peopleFind.forEachLine { if (it in listPhoneBook) count++  }
+    val timeToSearch = System.currentTimeMillis() - startTime
+    println("Found $count / 500 entries. Time taken: ${timeToSearch / 1000 / 60} min. ${ timeToSearch / 1000 % 60 } sec. ${timeToSearch % 1000} ms.")
 }
