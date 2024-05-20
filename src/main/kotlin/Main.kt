ff99b8f6c0a3faba8ca63cb0b4fdbf48ce9e349f
Nikita-Vesel0v <nikiveselov2004@gmail.com>
2024-05-20 12:04:28 +0300

    create function that count of people which we need to find in the phone book

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,10 @@
 import kotlin.io.path.Path
-
 import kotlin.io.path.forEachLine
-import kotlin.io.path.readLines
 import kotlin.io.path.readText
 
-fun main() {
-    val phoneBook = Path("src/main/resources/directory.txt")
-    val peopleFind = Path("src/main/resources/find.txt")
+fun countPeopleInPhoneBook(pathToBook: String, pathToListPeople: String) {
+    val phoneBook = Path(pathToBook)
+    val peopleFind = Path(pathToListPeople)
     val listPhoneBook = phoneBook.readText()
     var count = 0
     println("Start searching...")
@@ -14,4 +12,10 @@ fun main() {
     peopleFind.forEachLine { if (it in listPhoneBook) count++  }
     val timeToSearch = System.currentTimeMillis() - startTime
     println("Found $count / 500 entries. Time taken: ${timeToSearch / 1000 / 60} min. ${ timeToSearch / 1000 % 60 } sec. ${timeToSearch % 1000} ms.")
+}
+
+fun main() {
+    val pathToBook = "src/main/resources/directory.txt"
+    val pathToListPeople = "src/main/resources/find.txt"
+    countPeopleInPhoneBook(pathToBook, pathToListPeople)
 }
